1bad3a6c19191dfd9e7bf92ac8ec63ebddfc9e12
walterpadua <72560425+walterpadua@users.noreply.github.com>
2022-02-23 15:47:52 -0300

    salvando descrição readme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,9 +20,9 @@ class DatabaseSeeder extends Seeder
 
             try {
                 $this->call([
-                    // \Database\Seeders\Default\PessoaPessoasSeeder::class,
-                    // \Database\Seeders\Default\UsuarioUsuariosSeeder::class,
-                    \Database\Seeders\Default\CampeonatoCampeonatoSeeder::class,
+                    \Database\Seeders\Default\PessoaPessoasSeeder::class,
+                    \Database\Seeders\Default\UsuarioUsuariosSeeder::class,
+                    //\Database\Seeders\Default\CampeonatoCampeonatoSeeder::class,
                 ]);
                 DB::commit();
             } catch (Throwable $e) {
